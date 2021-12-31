7f80c13d689b5134d997704a756f42cd22a68045
Chue Chang <chuechang1996@outlook.com>
2021-12-31 02:06:55 -0600

    add notes in js

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 var timerEl = document.getElementById("timer")
 var startBtn = document.getElementById("start")
 var quizBodyEl = document.getElementById("main-body")
-var timeLeft = 500;
+var timeLeft = 50;
+// this is how much time player have to begin with. 
 var breakLine = document.createElement("br")
 var questions = [
     {
@@ -74,16 +75,23 @@ var questions = [
         option3: "3. create functions",
         answer: "3"
     }
+    // Questions are written in form of Object. This will come handy later down the road.
 ]
 startBtn.addEventListener("click", function () {
+    // adding an evenlistener for 'start quiz' button. when button is clicked, the function will execute. 
     var num = 0
+    //starting fuction at index 0 which begin with first question in array of questions define as object up above. 
     startQuiz(num)
+    //calling function to be execute
     function startQuiz(num) {
         var i = num
+        // variable i is set to num so options and questions in array can display properly. 
         quizBodyEl.textContent = ""
+        // setting content in main body to blink so it can add more content later on.
         var questionEl = document.createElement("ol")
         var answerEl = document.createElement("input")
         var submitEl = document.createElement("button")
+        // the code here are creating element in the html files. making it easier to hid and show.
         questionEl.textContent = questions[i].question
         option1 = document.createElement("li")
         option1.textContent = questions[i].option1
@@ -101,13 +109,19 @@ startBtn.addEventListener("click", function () {
         answerEl.setAttribute("placeholder", "Write your answer here")
         submitEl.textContent = "Submit"
         quizBodyEl.appendChild(submitEl)
+        // pretty much all the code above are creating what is neccessary to display the question, options, compare answer to input and button to submit.
         submitEl.addEventListener("click", function () {
+            // when button is clicked, answer is being compare and code execute accordly to rule. 
             if (answerEl.value == questions[i].answer) {
                 timeLeft += 15
+                // player gain 15 second to time if answer correctly.
                 num += 1
+                // this is to make sure next question will display.
                 if (num < questions.length) {
                     startQuiz(num)
+                    //rerunning the function if all question is not yet display.
                 } else {
+                    // if everything in array is displayed and answered. time will stop.
                     clearInterval(timer);
                     timerEl.textContent = timeLeft
                     quizBodyEl.textContent = "Your time stop at " + timeLeft + "."
@@ -118,23 +132,25 @@ startBtn.addEventListener("click", function () {
                     recordBtnEl.textContent = "RECORD"
                     quizBodyEl.appendChild(recordBtnEl)
                     recordBtnEl.addEventListener("click", function () {
-                        var highScore = localStorage.setItem(recordNameEl.value, timeLeft)
+                        localStorage.setItem(recordNameEl.value, timeLeft)
+                        // saving score and name onto local storage
                         var highScoreLink = document.getElementById("highscore")
-                        highScoreLink.addEventListener("click",function(){
-                            highScoreName = recordNameEl.value
-                            highScore = localStorage.getItem(highScoreName)
+                        highScoreLink.addEventListener("click", function () {
                             historyScore = JSON.stringify(localStorage.valueOf(localStorage))
                             quizBodyEl.textContent = historyScore
+                            //when score is clicked on top-left of page. score that is saved on local storage will show up.
                         })
                     })
                 }
             }
             else {
                 timeLeft -= 10
+                // if player answer incorrect or doesn't enter anything, player will lose 15 second. 
                 num += 1
                 if (num < questions.length) {
                     startQuiz(num)
                 } else {
+                    // code repeat as if player answer correctly and time have not run out.
                     clearInterval(timer);
                     timerEl.textContent = timeLeft
                     quizBodyEl.textContent = "Your time stop at " + timeLeft + "."
@@ -146,11 +162,9 @@ startBtn.addEventListener("click", function () {
                     quizBodyEl.appendChild(recordBtnEl)
                     recordBtnEl.addEventListener("click", function () {
                         highScoreName = recordNameEl.value
-                        var highScoreEl = localStorage.setItem(highScoreName, timeLeft)
+                        localStorage.setItem(highScoreName, timeLeft)
                         var highScoreLink = document.getElementById("highscore")
-                        highScoreLink.addEventListener("click",function(){
-                            highScoreName = recordNameEl.value
-                            highScore = localStorage.getItem(highScoreName)
+                        highScoreLink.addEventListener("click", function () {
                             historyScore = JSON.stringify(localStorage.valueOf(localStorage))
                             quizBodyEl.textContent = historyScore
                         })
@@ -161,12 +175,15 @@ startBtn.addEventListener("click", function () {
         })
     }
     var timer = setInterval(function () {
+        // with this code, timer is able to function. rules will apply if player run out of time by answering wrong answer or no answer.
         timerEl.textContent = timeLeft
         timeLeft--;
         if (timeLeft < 0) {
+            // time will stop if player cannot maintain time left. 
             clearInterval(timer);
             timerEl.textContent = 0
             quizBodyEl.textContent = "You ran out of time. You lose!"
+            // this will display if timer ran down to 0. everything stop and nothing else work even score. refesh to try again. 
         }
     }, 1000)
 })
